a4475783f8892bb9a92c7f38ad3e5166a40f9ba8
Vincent Song <songvc@users.noreply.github.com>
2016-05-02 22:25:22 -0700

    video detail components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
@@ -12,18 +13,16 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB2zwSfQkssqOkKvyzGJvYUPHBrHOgX5rY';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-  console.log(data);
-})
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { video: [] };
+    this.state = { videos: [] };
 
-    YTSearch({key:API_KEY, term: 'surfboards'}, (video) => {
-      this.setState({ videos: videos })
+    YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+      console.log(videos);
+      this.setState({ videos })
     });
   }
 
@@ -31,10 +30,12 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
-  
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
