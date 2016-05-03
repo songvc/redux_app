451dde5da110c5cfb6eb3b3ae8d20a7cbbaa5c54
Vincent Song <songvc@users.noreply.github.com>
2016-05-02 18:37:59 -0700

    tweak webpack & add components'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import App from './components/app';
+// import reducers from './reducers';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyB2zwSfQkssqOkKvyzGJvYUPHBrHOgX5rY';
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
