92ab0c8e0579151bc68ffeefdfd8c1ff56f8f646
Vincent Song <songvc@users.noreply.github.com>
2016-05-03 22:40:13 -0700

    fixed search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,9 +10,14 @@ class SearchBar extends Component {
     return (
       <div className="search-bar">
         <input value = {this.state.term}
-          onChange={(event) => this.setState({ term: event.target.value })}/>
+          onChange={(event) => this.onInputChange(event.target.value)}/>
       </div>
     )
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'Lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -23,7 +24,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
       console.log(videos);
       this.setState(
         {
@@ -35,9 +40,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
